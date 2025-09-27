62766e43b11cde50d9d1f86733566789d5f43b1e
RajmanderWebdesire <rajmander.dev@gmail.com>
2025-09-27 12:11:23 +0530

    changes in connection

--- dbConnect.js
+++ dbConnect.js
@@ -8,7 +8,7 @@ export const dbConnect = async () => {
   try {
     await mongoose.connect(
       //"mongodb+srv://rajmanderdev_db_user:ucbH5TamNuHzQbFe@rajcluster.k52h8mf.mongodb.net/demoapp?retryWrites=true&w=majority"
-      `mongodb+srv://${process.env.DB_USERNAME}:${DB_PASSWORD}@rajcluster.k52h8mf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@rajcluster.k52h8mf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     );
     console.log("Connection success");
   } catch (err) {
