3f95e8d2a82b47814946658d45235cba8245592e
RajmanderWebdesire <rajmander.dev@gmail.com>
2025-09-27 11:48:21 +0530

    changes

--- dbConnect.js
+++ dbConnect.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const dbConnect = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://rajmanderdev_db_user:ucbH5TamNuHzQbFe@rajcluster.k52h8mf.mongodb.net/demoapp?retryWrites=true&w=majority"
+      //"mongodb+srv://rajmanderdev_db_user:ucbH5TamNuHzQbFe@rajcluster.k52h8mf.mongodb.net/demoapp?retryWrites=true&w=majority"
+      `mongodb+srv://${process.env.DB_USERNAME}:${DB_PASSWORD}@rajcluster.k52h8mf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     );
     console.log("Connection success");
   } catch (err) {
